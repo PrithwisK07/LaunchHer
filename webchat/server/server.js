c06ddd1414055f957166ec54b02bd7c5b88ba24b
PrithwisK07 <karmakarprithwis566@gmail.com>
2026-05-26 09:59:52 +0530

    fix cors url

--- webchat/server/server.js
+++ webchat/server/server.js
@@ -7,7 +7,7 @@ import { filterSchemes, synthesizeResponse } from "./engine.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
